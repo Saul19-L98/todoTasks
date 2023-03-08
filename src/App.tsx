a9adcf8279c9c40fa750907970d0668dd1b17460
sallanez <saul.alejandro19@gmail.com>
2023-03-07 18:36:26 -0600

    test2 error

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,18 @@ import {useState} from 'react';
 import Modal from 'react-modal';
 import FormModal from './components/FormModal';
 import TasksView from './components/TasksView';
-import Footer from './components/Footer';
+// import Footer from './components/Footer';
 
-const customStyles = {
-  content: {
-    top: '50%',
-    left: '50%',
-    right: 'auto',
-    bottom: 'auto',
-    marginRight: '-50%',
-    transform: 'translate(-50%, -50%)',
-  },
-};
+// const customStyles = {
+//   content: {
+//     top: '50%',
+//     left: '50%',
+//     right: 'auto',
+//     bottom: 'auto',
+//     marginRight: '-50%',
+//     transform: 'translate(-50%, -50%)',
+//   },
+// };
 
 Modal.setAppElement(document.getElementById('root')! as HTMLDivElement);
 
